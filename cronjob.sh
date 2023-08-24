ea47849e7c6c8057913e5dc739b2a5d56b482093
DuttonMa <martin@theduttons.org>
2023-08-24 15:26:12 +0100

    set up pacni logging

--- cronjob.sh
+++ cronjob.sh
@@ -17,3 +17,4 @@
 /bin/bash /app/nicsdru-logging/scripts/shiplog.sh "drupal-lgbcni" "/app/log/lgbcni/drupal.log" "$(date +'%a, %d/%m/%Y -')" "drupal"
 /bin/bash /app/nicsdru-logging/scripts/shiplog.sh "drupal-semcommittee" "/app/log/semcommittee/drupal.log" "$(date +'%a, %d/%m/%Y -')" "drupal"
 /bin/bash /app/nicsdru-logging/scripts/shiplog.sh "drupal-boundarycommission" "/app/log/boundarycommission/drupal.log" "$(date +'%a, %d/%m/%Y -')" "drupal"
+/bin/bash /app/nicsdru-logging/scripts/shiplog.sh "drupal-pacni" "/app/log/pacni/drupal.log" "$(date +'%a, %d/%m/%Y -')" "drupal"
