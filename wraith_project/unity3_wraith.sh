99bb264b21507e138749cbaf984ef11e3e2c7d1a
DuttonMa <duttonma@gmail.com>
2024-02-29 16:37:13 +0000

    Add tests for lgbc

--- wraith_project/unity3_wraith.sh
+++ wraith_project/unity3_wraith.sh
@@ -5,6 +5,7 @@ wraith capture configs/pacni_capture.yaml >> output.txt
 wraith capture configs/interchange_capture.yaml >> output.txt
 wraith capture configs/parole_capture.yaml >> output.txt
 wraith capture configs/library_capture.yaml >> output.txt
+wraith capture configs/lgbc_capture.yaml >> output.txt
 grep -A3 'failed' output.txt
 echo '************'
 grep gallery output.txt
