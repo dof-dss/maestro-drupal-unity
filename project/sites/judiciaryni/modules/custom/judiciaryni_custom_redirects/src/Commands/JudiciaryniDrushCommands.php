753544a8f57177f43f657fd68d1566c80f885d20
DuttonMa <duttonma@gmail.com>
2024-03-04 16:43:03 +0000

    generate redirects more selectively

--- project/sites/judiciaryni/modules/custom/judiciaryni_custom_redirects/src/Commands/JudiciaryniDrushCommands.php
+++ project/sites/judiciaryni/modules/custom/judiciaryni_custom_redirects/src/Commands/JudiciaryniDrushCommands.php
@@ -3,6 +3,7 @@
 namespace Drupal\judiciaryni_custom_redirects\Commands;
 
 use Drupal\structure_sync\StructureSyncHelper;
+use Drupal\redirect\Entity\Redirect;
 use Drush\Commands\DrushCommands;
 
 /**
@@ -37,12 +38,31 @@ class JudiciaryniDrushCommands extends DrushCommands {
       ->accessCheck(FALSE)
       ->execute();
     $files = $storage->loadMultiple($fids);
-    $titles = [];
+    $total = 0;
     foreach ($files as $file) {
-      $url = $file->createFileUrl();
-      print("Path is " . $url . "\n");
-      $oldpath = str_replace('files/judiciaryni','sites/judiciary/files',$url);
-      print("Old path is " . $oldpath . "\n\n");
+      $url = urldecode($file->createFileUrl());
+      $oldpath = str_replace('files/judiciaryni/decisions','sites/judiciary/files/decisions',$url);
+      $oldpath = str_replace('files/judiciaryni/media-files','sites/judiciary/files/media-files',$url);
+      // Check to see if a redirect already exists.
+      $redirects = $this->entityTypeManager->getStorage('redirect')->getQuery()
+        ->accessCheck(TRUE)
+        ->condition('redirect_source.path', substr($oldpath,1))
+        ->execute();
+      if (empty($redirects)) {
+        // Create the new redirect.
+        Redirect::create([
+          'redirect_source' => substr($oldpath,1),
+          'redirect_redirect' => 'internal:' . $url,
+          'language' => 'und',
+          'status_code' => '301',
+        ])->save();
+        $total++;
+      }
+    }
+    if ($total > 0) {
+      print("Successfully created $total redirects\n");
+    } else {
+      print("Did not create any redirects (perhaps they already exist ?\n");
     }
   }
 }
